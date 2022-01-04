9fa5acbcf9ffd83f84e05e0f93fcf8cbf6141d05
Diego <you@example.com>
2022-01-04 13:08:43 -0300

    Criado Card Game

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 import './src/styles/settings/colors.css';
 import './src/styles/elements/base.css';
 import CardGame from "./src/components/CardGame";
+import BoardGame from './src/components/objetct/BoardGame';
 
 // document.querySelector('#app').innerHTML = `
 //   <h1>Hello Vite!</h1>
 //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 // `
 const $root = document.querySelector("#root");
-const $htmlCardGame = CardGame();
+const $htmlBoardGame = BoardGame(3);
 
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
+$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
