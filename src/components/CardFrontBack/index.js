9a0ef5a855a0af646cb3c13e5503684d94ed4b31
Diego Pollheim <diegopollheim@gmail.com>
2022-01-05 22:16:33 -0300

    Iniciado cards um atrás do outro

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,15 @@
 import CardGame from "../CardGame";
+import "./style.css";
 
 export default function CardFrontBack() {
   return /*html*/ `
     <article class="card-front-back">
-      ${CardGame()}
-      ${CardGame("javascript", "Logo do JavaScript")}
+        <div class="front">
+            ${CardGame()}
+        </div>
+        <div class="back">
+          ${CardGame("javascript", "Logo do JavaScript")}
+        </div>       
     </article>
     `;
 }
