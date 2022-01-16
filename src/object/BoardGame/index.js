826c1d88ce50e415dad98d4751d918c76f29e248
Diego Pollheim <diegopollheim@gmail.com>
2022-01-15 21:03:18 -0300

    troca do marcador de pontos dos player ao viras duas cartas

--- src/object/BoardGame/index.js
+++ src/object/BoardGame/index.js
@@ -2,23 +2,41 @@
 import CardFrontBack from "../../components/CardFrontBack";
 import "./style.css";
 import cards from "./data.js";
+import SetaPlacar from "../../components/SetaPlacar";
 
 export default function BoardGame(amountCards) {
 
   //CONTEXTO PARA FUNCÃO
   window.boardGame = {};
   window.boardGame.handleClick = () => {
-    console.log("Clicou");
 
+    // ELEMENTO DA SETA
+    const $setaPlayer = document.querySelector(".seta-point");
+
+    // CARDS VIRADOS
     const $cardsVirados = document.querySelectorAll(".active");
+
+    const HideCards = () => {
+      $cardsVirados.forEach((card) => card.classList.remove("active"));
+    }
+    
+    const AlteraSetaPlacar = () =>{
+      const currentPlayer = $setaPlayer.getAttribute("data-currentplayer");
+      $setaPlayer.setAttribute("data-currentplayer", currentPlayer === "1" ? "2" : 1)
+    }
+
     if ($cardsVirados.length === 2) {
       setTimeout(function () {
-        $cardsVirados.forEach((card) => card.classList.remove("active"))
-      }, 600);
-    }
-    console.log(cardsVirados.length);
-  }
+        
+        // VIRA E DESVIRA AS CARTAS
+        HideCards();
 
+        // ALTERA A SETA ENTRE OS PLAYERS
+       AlteraSetaPlacar()
+      }, 600);
+
+    }
+  }
 
   const $htmlCards = cards.map((card) => CardFrontBack(card.icon, card.altIcon));
   const $htmlCardsList = $htmlCards.join("");
